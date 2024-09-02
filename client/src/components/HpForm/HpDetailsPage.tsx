29f981373fc491b807be979d769f4a38825fbb95
Aditya Vardhan Singh <aditya.vardhan.singh@outlook.com>
2024-09-02 19:49:16 +0530

    Tournament Form: Store in hook and pass to backend

--- client/src/components/HpForm/HpDetailsPage.tsx
+++ client/src/components/HpForm/HpDetailsPage.tsx
@@ -1,44 +1,117 @@
-import React from 'react';
-import { TextInput, Textarea, Container, Grid, Button, Group } from '@mantine/core';
-import styles from './HpDetailsPage.module.css';
+import {
+  Button,
+  Container,
+  Grid,
+  Group,
+  TextInput,
+  Textarea,
+} from "@mantine/core";
+import React from "react";
+import styles from "./HpDetailsPage.module.css";
 
 interface NavProps {
   page: string;
   setPage: React.Dispatch<React.SetStateAction<string>>;
+  details: Details;
+  setDetails: React.Dispatch<React.SetStateAction<Details>>;
 }
 
-function HpDetailsPage({ page, setPage }: NavProps) {
+interface Details {
+  tournamentName: string;
+  startDate: string;
+  endDate: string;
+  venueName: string;
+  city: string;
+  tournamentDetails: string;
+}
+
+function HpDetailsPage({ page, setPage, details, setDetails }: NavProps) {
+  const today = new Date().toISOString().split("T")[0];
   return (
     <Container mt={30} className={styles.formContainer}>
       <Grid>
         <Grid.Col span={12}>
-          <TextInput label="Tournament Name" placeholder="Enter tournament Name" required />
+          <TextInput
+            type="text"
+            autoComplete="tournament-name"
+            label="tournament-name"
+            placeholder="Enter tournament Name"
+            required
+            value={details.tournamentName}
+            onChange={(e) =>
+              setDetails({ ...details, tournamentName: e.target.value })
+            }
+          />
         </Grid.Col>
         <Grid.Col span={6}>
-          <TextInput label="Start Date" placeholder="DD/MM/YYYY" required />
+          <TextInput
+            type="date"
+            min={today}
+            max={
+              new Date(new Date().setFullYear(new Date().getFullYear() + 2))
+                .toISOString()
+                .split("T")[0]
+            }
+            label="Start Date"
+            placeholder="DD/MM/YYYY"
+            required
+            value={details.startDate}
+            onChange={(e) =>
+              setDetails({ ...details, startDate: e.target.value })
+            }
+          />
         </Grid.Col>
         <Grid.Col span={6}>
-          <TextInput label="End Date" placeholder="DD/MM/YYYY" required />
+          <TextInput
+            type="date"
+            min={details.startDate}
+            max={new Date(new Date().setDate(new Date().getDate() + ))}
+            label="End Date"
+            placeholder="DD/MM/YYYY"
+            required
+            value={details.endDate}
+            onChange={(e) =>
+              setDetails({ ...details, endDate: e.target.value })
+            }
+          />
         </Grid.Col>
         <Grid.Col span={12}>
-          <TextInput label="Venue Name" placeholder="Enter the venue name" required />
+          <TextInput
+            label="Venue Name"
+            placeholder="Enter the venue name"
+            required
+            value={details.venueName}
+            onChange={(e) =>
+              setDetails({ ...details, venueName: e.target.value })
+            }
+          />
         </Grid.Col>
         <Grid.Col span={12}>
-          <TextInput label="City" placeholder="Enter City" required />
+          <TextInput
+            label="City"
+            placeholder="Enter City"
+            required
+            value={details.city}
+            onChange={(e) => setDetails({ ...details, city: e.target.value })}
+          />
         </Grid.Col>
         <Grid.Col span={12}>
           <Textarea
             label="Tournament Details"
             placeholder="Provide details of the tournament"
             required
+            value={details.tournamentDetails}
+            onChange={(e) =>
+              setDetails({ ...details, tournamentDetails: e.target.value })
+            }
           />
         </Grid.Col>
       </Grid>
       <Group mt={50}>
-        <Button onClick={() => setPage('')} color="#058A4A">
+        <Button onClick={() => setPage("")} color="#058A4A">
           Prev
         </Button>
-        <Button onClick={() => setPage('LinksPage')} color="#058A4A">
+        <Button onClick={() => setPage("LinksPage")} color="#058A4A">
           Next
         </Button>
       </Group>
--- client/src/components/HpForm/HpSchedulePage.tsx
+++ client/src/components/HpForm/HpSchedulePage.tsx
@@ -1,34 +1,53 @@
-import React, { useState } from 'react';
-import { TextInput, Container, Grid, Button, Group } from '@mantine/core';
-import styles from './HpDetailsPage.module.css';
+import React, { useState } from "react";
+import { TextInput, Container, Grid, Button, Group } from "@mantine/core";
+import styles from "./HpDetailsPage.module.css";
 
-interface ScheduleData {
+interface NavProps {
+  page: string;
+  setPage: React.Dispatch<React.SetStateAction<string>>;
+  schedules: Schedule[];
+  setSchedules: React.Dispatch<React.SetStateAction<Schedule[]>>;
+}
+
+interface Schedule {
   scheduleName: string;
   startDate: string;
   startTime: string;
   endDate: string;
   endTime: string;
 }
-interface NavProps {
-  page: string;
-  setPage: React.Dispatch<React.SetStateAction<string>>;
-}
 
-function HpSchdulePage({ page, setPage }: NavProps) {
-  const [schedules, setSchedules] = useState<ScheduleData[]>([
-    { scheduleName: '', startDate: '', startTime: '', endDate: '', endTime: '' },
-  ]);
+function HpSchdulePage({ page, setPage, schedules, setSchedules }: NavProps) {
+  // const [schedules, setSchedules] = useState<ScheduleData[]>([
+  //   {
+  //     scheduleName: "",
+  //     startDate: "",
+  //     startTime: "",
+  //     endDate: "",
+  //     endTime: "",
+  //   },
+  // ]);
 
   const handleAddSchedule = () => {
     setSchedules([
       ...schedules,
-      { scheduleName: '', startDate: '', startTime: '', endDate: '', endTime: '' },
+      {
+        scheduleName: "",
+        startDate: "",
+        startTime: "",
+        endDate: "",
+        endTime: "",
+      },
     ]);
   };
 
-  const handleChange = (index: number, field: keyof ScheduleData, value: string) => {
+  const handleChange = (
+    index: number,
+    field: keyof Schedule,
+    value: string,
+  ) => {
     const newSchedules = schedules.map((schedule, i) =>
-      i === index ? { ...schedule, [field]: value } : schedule
+      i === index ? { ...schedule, [field]: value } : schedule,
     );
     setSchedules(newSchedules);
   };
@@ -45,56 +64,72 @@ function HpSchdulePage({ page, setPage }: NavProps) {
           <Grid>
             <Grid.Col span={6}>
               <TextInput
-                label="Schedule Name"
+                type="text"
+                autoComplete="schedule-name"
+                label="schedule-name"
                 placeholder="Enter the official tournament event name"
                 required
                 value={schedule.scheduleName}
-                onChange={(e) => handleChange(index, 'scheduleName', e.target.value)}
+                onChange={(e) =>
+                  handleChange(index, "scheduleName", e.target.value)
+                }
               />
             </Grid.Col>
 
             <Grid.Col span={3}>
               <TextInput
-                label="Start Date"
+                type="date"
+                label="start-date"
                 placeholder="DD/MM/YYYY"
                 required
                 value={schedule.startDate}
-                onChange={(e) => handleChange(index, 'startDate', e.target.value)}
+                onChange={(e) =>
+                  handleChange(index, "startDate", e.target.value)
+                }
               />
             </Grid.Col>
 
             <Grid.Col span={3}>
               <TextInput
-                label="Start Time"
+                type="date"
+                label="start-time"
                 placeholder="HH:MM"
                 required
                 value={schedule.startTime}
-                onChange={(e) => handleChange(index, 'startTime', e.target.value)}
+                onChange={(e) =>
+                  handleChange(index, "startTime", e.target.value)
+                }
               />
             </Grid.Col>
 
             <Grid.Col span={3}>
               <TextInput
-                label="End Date"
+                type="date"
+                label="end-date"
                 placeholder="DD/MM/YYYY"
                 required
                 value={schedule.endDate}
-                onChange={(e) => handleChange(index, 'endDate', e.target.value)}
+                onChange={(e) => handleChange(index, "endDate", e.target.value)}
               />
             </Grid.Col>
 
             <Grid.Col span={3}>
               <TextInput
-                label="End Time"
+                type="date"
+                label="end-time"
                 placeholder="HH:MM"
                 required
                 value={schedule.endTime}
-                onChange={(e) => handleChange(index, 'endTime', e.target.value)}
+                onChange={(e) => handleChange(index, "endTime", e.target.value)}
               />
             </Grid.Col>
 
             <Grid.Col span={6}>
-              <Button variant="outline" color="red" onClick={() => handleDeleteSchedule(index)}>
+              <Button
+                variant="outline"
+                color="red"
+                onClick={() => handleDeleteSchedule(index)}
+              >
                 Delete Schedule
               </Button>
             </Grid.Col>
@@ -112,10 +147,16 @@ function HpSchdulePage({ page, setPage }: NavProps) {
         Add Schedule
       </Button>
       <Group mt={50}>
-        <Button onClick={() => setPage('PrizesPage')} color="#058A4A">
+        <Button onClick={() => setPage("PrizesPage")} color="#058A4A">
           Prev
         </Button>
-        <Button onClick={() => setPage('SubmitPage')} color="#058A4A">
+        <Button
+          onClick={() => {
+            setPage("SubmitPage");
+            console.log(schedules);
+          }}
+          color="#058A4A"
+        >
           Next
         </Button>
       </Group>
